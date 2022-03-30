90106f0e0d59838bd9c92136a05a7102b3edf552
Oceanenjoyer <oceanenjoyer@protonmail.com>
2022-03-30 10:12:41 +0200

    Dodano szyfr cezara

---
+++ szyfr_cezara/szyfr_cezara.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+using namespace std;
+void szyfrowanie();
+
+int main()
+{
+	int klucz;
+	char tab[200];
+
+	cin >> klucz;
+
+	cin >> tab;
+
+	szyfrowanie();
+}
+
+void szyfrowanie(int klucz, char tab[])
+{
+	int dl = strlen(tab);
+
+	if (!(klucz >= -26 && klucz <= 26))
+	{
+		return;
+	}
+
+	if (klucz >= 0)
+	{
+		for (int i = 0; i < dl; i++)
+		{
+			if (tab + klucz <= "z")
+			{
+				tab += klucz;
+			}
+			else
+			{
+				tab = tab + klucz - 26;
+			}
+		}
+	}
+	else
+	{
+		for (int i = 0; i < dl; i++)
+		{
+			if (tab + klucz >= "a")
+			{
+				tab += klucz;
+			}
+			else
+			{
+				tab = tab + klucz + 26;
+			}
+		}
+	}
+}
